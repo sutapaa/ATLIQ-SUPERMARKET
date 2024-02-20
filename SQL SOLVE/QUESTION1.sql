d6c8da3057ce5c1459d9c53af0167f8e054139d2
Sutapa Ghosh <141821636+sutapaa@users.noreply.github.com>
2024-02-20 11:19:34 +0530

    Delete SQL SOLVE directory

--- SQL SOLVE/QUESTION1.sql
+++
@@ -1,7 +0,0 @@
--- 1 NO QUESTION
-select distinct product_name,base_price,promo_type 
-from retail_events_db.dim_products  p join retail_events_db.fact_events fe
-on  fe.product_code=p.product_code where 
-base_price>500 and promo_type="BOGOF";
-
-
--- SQL SOLVE/QUESTION2.sql
+++
@@ -1,5 +0,0 @@
--- 2 NO QUESTION
-select city,count(store_id) as store_count
-from retail_events_db.dim_stores  
-group by city 
-order by store_count desc;
